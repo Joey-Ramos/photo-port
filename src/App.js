caf88d75b054dcd63387df43e49c0ac85a347b5c
Joey Ramos <joey@Joeys-Mac-mini.local>
2020-09-30 18:05:09 -0500

    updated tests for nav and added test to gallery and photolist

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ function App() {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life'},
+    { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses waterfalls, and the beauty of nature' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav>
+      <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
+      ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
